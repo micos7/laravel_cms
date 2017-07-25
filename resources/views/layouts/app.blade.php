a6e1b08f954024d5f56b7f091e750be3d1459112
micos7 <micos7@gmail.com>
2017-07-25 22:04:33 +0300

    Fixed a nav bug.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,43 +15,64 @@
 </head>
 <body>
     <div id="app">
-        <nav class="nav has-shadow">
-            <div class="container">
-            <div class="nav-left">
-            <a href={{ route('home')}} class="nav-item">
-            <img src="{{asset('images/logo')}} " alt="LaravelCMS" />
-            </a>
-            <a href="" class="nav-item is-tab is-hidden-mobile m-l-10">Learn</a>
-            <a href="" class="nav-item is-tab is-hidden-mobile">Discuss</a>
-            <a href="" class="nav-item is-tab is-hidden-mobile">Share</a>
-            </div>
-            </div>
-            <div class="nav-right">
-            @if(!Auth::guest())
-                <a href="" class="nav-item is-tab">Login</a>
-                <a href="" class="nav-item is-tab">Join the community</a>
-            @else
-                <button class="dropdown nav-item is-tab">
-                Hey Mihai! <span class="icon"><i class="fa fa-caret-down"></i></span>
-                <ul class="dropdown-menu">
-                    <li><a href="">
-                    <span class="icon"><i class="fa fa-fw m-r-5 fa-user-circle-o"></i></span>
-                    Profile</a></li>
-                    <li><a href="">
-                    <span class="icon"><i class="fa fa-fw m-r-5  fa-bell"></i></span>
-                    Notifications</a></li>
-                    <li><a href="">
-                    <span class="icon"><i class="fa fa-fw m-r-5  fa-cog"></i></span>
-                    Settings</a></li>
-                    <li class="separator"></li>
-                    <li><a href="">
-                    <span class="icon"><i class="fa fa-fw m-r-5  fa-sign-out"></i></span>
-                    Logout</a></li>
-                </ul>
-                </button>
-            @endif
-            </div>
-        </nav>
+        <nav class="nav has-shadow" >
+  <div class="container">
+    <div class="nav-left">
+      <a class="nav-item is-paddingless" href="{{route('home')}}">
+        <img src="{{asset('images/devmarketer-logo.png')}}" alt="DevMarketer logo">
+      </a>
+      <a class="nav-item is-tab is-hidden-mobile m-l-10">Learn</a>
+      <a class="nav-item is-tab is-hidden-mobile">Discuss</a>
+      <a class="nav-item is-tab is-hidden-mobile">Share</a>
+    </div>
+    <span class="nav-toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+    <div class="nav-right nav-menu" style="overflow: visible">
+      <a class="nav-item is-tab is-hidden-tablet is-active">Learn</a>
+      <a class="nav-item is-tab is-hidden-tablet">Discuss</a>
+      <a class="nav-item is-tab is-hidden-tablet">Share</a>
+      @if (Auth::guest())
+        <a href="{{route('login')}}" class="nav-item is-tab">Login</a>
+        <a href="{{route('register')}}" class="nav-item is-tab">Join the Community</a>
+      @else
+        <button class="dropdown is-aligned-right nav-item is-tab" >
+          Hey {{ Auth::user()->name }}
+          <ul class="dropdown-menu" style="overflow: visible;">
+            <li><a href="#">
+                  <span class="icon">
+                    <i class="fa fa-fw fa-user-circle-o m-r-5"></i>
+                  </span>Profile
+                </a>
+            </li>
+            <li><a href="#">
+                  <span class="icon">
+                    <i class="fa fa-fw fa-bell m-r-5"></i>
+                  </span>Notifications
+                </a>
+            </li>
+            <li><a href="{{route('manage.dashboard')}}">
+                  <span class="icon">
+                    <i class="fa fa-fw fa-cog m-r-5"></i>
+                  </span>Manage
+                </a>
+            </li>
+            <li class="seperator"></li>
+            <li><a href="{{route('logout')}}" >
+                  <span class="icon">
+                    <i class="fa fa-fw fa-sign-out m-r-5"></i>
+                  </span>
+                  Logout
+                </a>
+            </li>
+          </ul>
+        </button>
+      @endif
+    </div>
+  </div>
+</nav>
 
         @yield('content')
     </div>
