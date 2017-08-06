1ddaff5b270690531f19c8e95022236d9952f276
micos7 <micos7@gmail.com>
2017-08-06 22:32:36 +0300

    Finish user CRUD.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use DB;
+use Session;
+use Hash;
+
 
 class UserController extends Controller
 {
@@ -36,14 +40,13 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' =>'required|max:255',
-            'email' =>'required|email|unique:users'
-        ]);
-
-        if (Request::has('password')  && !empty($request->$password)) {
-             $password = trim($request->password);
-        } else {
+      $this->validate($request, [
+        'name' => 'required|max:255',
+        'email' => 'required|email|unique:users'
+      ]);
+      if (!empty($request->password)) {
+        $password = trim($request->password);
+      } else {
         # set the manual password
         $length = 10;
         $keyspace = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
@@ -54,14 +57,21 @@ class UserController extends Controller
         }
         $password = $str;
       }
-
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($password);
-        $user->save();
-
+      $user = new User();
+      $user->name = $request->name;
+      $user->email = $request->email;
+      $user->password = Hash::make($password);
+      $user->save();
+      if ($request->roles) {
+        $user->syncRoles(explode(',', $request->roles));
+      }
       return redirect()->route('users.show', $user->id);
+      // if () {
+      //
+      // } else {
+      //   Session::flash('danger', 'Sorry a problem occurred while creating this user.');
+      //   return redirect()->route('users.create');
+      // }
     }
 
     /**
@@ -72,7 +82,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::findOrFail($id);
+        return view('manage.users.show')->withUser($user);
     }
 
     /**
@@ -83,7 +94,8 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::findOrFail($id);
+        return view('manage.users.edit')->withUser($user);
     }
 
     /**
@@ -95,7 +107,34 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+         $this->validate($request, [
+        'name' => 'required|max:255',
+        'email' => 'required|email|unique:users,email,'.$id
+      ]);
+      $user = User::findOrFail($id);
+      $user->name = $request->name;
+      $user->email = $request->email;
+      if ($request->password_options == 'auto') {
+        $length = 10;
+        $keyspace = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+        $str = '';
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= $keyspace[random_int(0, $max)];
+        }
+        $user->password = Hash::make($str);
+      } elseif ($request->password_options == 'manual') {
+        $user->password = Hash::make($request->password);
+      }
+      $user->save();
+    //   $user->syncRoles(explode(',', $request->roles));
+      return redirect()->route('users.show', $id);
+      // if () {
+      //   return redirect()->route('users.show', $id);
+      // } else {
+      //   Session::flash('error', 'There was a problem saving the updated user info to the database. Try again later.');
+      //   return redirect()->route('users.edit', $id);
+      // }
     }
 
     /**
