8620f3e2762be98535fd7daa3b588f3e5f75d41d
micos7 <micos7@gmail.com>
2017-08-06 17:39:53 +0300

    Create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('manage.users.create');
     }
 
     /**
@@ -36,7 +36,32 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' =>'required|max:255',
+            'email' =>'required|email|unique:users'
+        ]);
+
+        if (Request::has('password')  && !empty($request->$password)) {
+             $password = trim($request->password);
+        } else {
+        # set the manual password
+        $length = 10;
+        $keyspace = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+        $str = '';
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= $keyspace[random_int(0, $max)];
+        }
+        $password = $str;
+      }
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($password);
+        $user->save();
+
+      return redirect()->route('users.show', $user->id);
     }
 
     /**
